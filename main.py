6b7ec0b75ada78e0d09015bfd8fbd600c6d9e6aa
Getulio-Mendes <mendes.getulio@outlook.com>
2023-07-03 12:57:59 -0300

    Mudança de direção completa

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Body:
         self.changed = False
 
     def print(self):
+        print("\t------ {} ------".format(self.name))
         print("Velocidade x: {}".format(self.vx))
         print("Velocidade y: {}".format(self.vy))
         print("Massa : {}".format(self.mass))
@@ -51,14 +52,14 @@ class Body:
     def changeVel(self,x2,y2,SCALE):
 
         if self.changed == False:
-            x1 = self.x * SCALE + WIDTH / 2
-            y1 = self.y * SCALE + HEIGHT / 2
-        
-            theta = math.atan2(x1 - x2, y1 - y2)
-            mod = math.dist([x1,y1],[x2,y2])
+            x1 = self.x * SCALE + WIDTH/2
+            y1 = self.y * SCALE + HEIGHT/2
 
-            self.vx = self.vx + math.cos(theta) * mod * SCALE
-            self.vy = self.vy + math.sin(theta) * mod * SCALE
+            theta = math.atan2(y2 - y1, x2 - x1)
+            mod = math.dist([x2,y2],[x1,y1])
+
+            self.vx = self.vx + math.cos(theta) * mod * 100
+            self.vy = self.vy + math.sin(theta) * mod * 100
             self.changed = True
 
 
@@ -79,15 +80,6 @@ class Body:
         self.y += self.vy * TIMESTEP
         self.trace.append((self.x, self.y))
         self.changed = False
-
-PI = 3.14159265358979323
-AU = 149.6e6 * 1000
-G = 6.67428e-11
-TIMESTEP = 3600*24
-WHITE = (255, 255,255)
-
-WIDTH = 800
-HEIGHT = 600
 
 class gVar:
 
@@ -112,17 +104,39 @@ class gVar:
     def pause(self):
         self.PAUSE = not self.PAUSE
 
+
+PI = 3.14159265358979323
+AU = 149.6e6 * 1000
+G = 6.67428e-11
+TIMESTEP = 3600*24
+WHITE = (255, 255,255)
+
+WIDTH = 800
+HEIGHT = 600
+
+def bodiesInit():
+    sun = Body(0, 0,0,0,1.98892 * 10**30,WHITE,50,"Sol")
+
+    # position = AU
+    # vel = m/s
+    # mass = kg
+
+    earth = Body(-1 * AU, 0,0,29.783 * 1000,5.9742 * 10**24,WHITE,10,"Terra")
+    mars = Body(-1.524 * AU, 0,0,24.077 * 1000, 6.39 * 10**23,WHITE,10,"Marte")
+    mercury = Body(0.387 * AU, 0,0,-47.4 * 1000,3.30 * 10**23,WHITE,10,"Mercúrio")
+    venus = Body(0.723 * AU, 0,0,-35.02 * 1000,4.8685 * 10**24,WHITE,10,"Vênus")
+
+    return [sun, earth,mars,mercury,venus]
+
+
 def checkCollision(x1,y1,r1,x2,y2,r2):
     if(math.dist([x1,y1],[x2,y2]) < r1 + r2):
         return True
 
 def velArrow(win,const,arrow):
-    x = arrow[0] * const.SCALE + WIDTH / 2
-    y = arrow[1] * const.SCALE + HEIGHT / 2
-
-    # Setinha
-
-    draw_arrow(win,pygame.Vector2(x,y),pygame.Vector2(arrow[2],arrow[3]),pygame.Color("red"),4,12,12)
+    start = pygame.Vector2(arrow[2],arrow[3])
+    end = pygame.Vector2(arrow[0],arrow[1])
+    draw_arrow(win,start,end,pygame.Color("red"),4/const.ZOOM,12/const.ZOOM,12/const.ZOOM)
 
 
 def main(ref='sun'):
@@ -137,19 +151,9 @@ def main(ref='sun'):
     pygame.display.set_caption("Simulador de Gravidade")
 
 
-    sun = Body(0, 0,0,0,1.98892 * 10**30,WHITE,50,"Sol")
-
-    # position = AU
-    # vel = m/s
-    # mass = kg
-
-    earth = Body(-1 * AU, 0,0,29.783 * 1000,5.9742 * 10**24,WHITE,10,"Terra")
-    mars = Body(-1.524 * AU, 0,0,24.077 * 1000, 6.39 * 10**23,WHITE,10,"Marte")
-    mercury = Body(0.387 * AU, 0,0,-47.4 * 1000,3.30 * 10**23,WHITE,10,"Mercúrio")
-    venus = Body(0.723 * AU, 0,0,-35.02 * 1000,4.8685 * 10**24,WHITE,10,"Vênus")
-
-    bodies = [sun, earth,mars,mercury,venus]
+    bodies = bodiesInit()
     mouseMotion = None
+    dragged = False
     arrows = []
 
     while run:
@@ -166,21 +170,22 @@ def main(ref='sun'):
                 if event.key == pygame.K_SPACE:
                     arrows = []
                     const.pause()
+                elif event.key == pygame.K_ESCAPE:
+                    bodies = bodiesInit()
 
             elif event.type == pygame.MOUSEBUTTONUP:
                 if const.PAUSE:
-                    if(dragged):
-
+                    if(dragged and mouseMotion[2].changed == False):
                         arrows.append((event.pos[0],event.pos[1],mouseMotion[0],mouseMotion[1],mouseMotion[2]))
 
-                        mouseMotion = None
-                        dragged = False
+                    mouseMotion = None
+                    dragged = False
             
             elif event.type == pygame.MOUSEMOTION:
                 if mouseMotion != None:
-                    mouseMotion[0] = event.pos[0]
-                    mouseMotion[1] = event.pos[1]
-                    dragged = True
+                    if (math.fabs(event.pos[0] - mouseMotion[0]) > 10):
+                        if(math.fabs(event.pos[1] - mouseMotion[1]) > 10):
+                            dragged = True
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 5:
@@ -190,23 +195,24 @@ def main(ref='sun'):
                     const.addZoom(0,bodies)
 
                 elif event.button == 1:
-                    for bodie in bodies:
-                        x = bodie.x * const.SCALE + WIDTH / 2
-                        y = bodie.y * const.SCALE + HEIGHT / 2
-                        if(checkCollision(x,y,bodie.radius/const.ZOOM,event.pos[0],event.pos[1],1)):
-                            bodie.print()
+                    for body in bodies:
+                        x = body.x * const.SCALE + WIDTH /2
+                        y = body.y * const.SCALE + HEIGHT /2
+                        if(checkCollision(x,y,body.radius/const.ZOOM,event.pos[0],event.pos[1],1)):
+                            body.print()
                             # Seleciona o corpo cliclado
-                            mouseMotion = [event.pos[0],event.pos[1],bodie.name]
+                            mouseMotion = [x,y,body]
+
+        for arrow in arrows:
+            velArrow(win,const,arrow) 
 
         for body in bodies:
             if not const.PAUSE:
                 body.updatePosition(bodies)
 
             for arrow in arrows:
-                velArrow(win,const,arrow) 
-
-                if arrow[4] == bodie.name:
-                    bodie.changeVel(arrow[2],arrow[3],const.SCALE)
+                if arrow[4].name == body.name:
+                    body.changeVel(arrow[0],arrow[1],const.SCALE)
 
             body.draw(win,const.ZOOM,const.SCALE)
 
